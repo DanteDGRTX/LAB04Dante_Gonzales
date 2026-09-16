ba1ef41e166290540459380f8cea09efdf3811db
Ronal Dante Gonzales Quispe <ronal.gonzales@tecsup.edu.pe>
2026-09-15 21:58:44 -0500

    feat: Add Card component

--- app/src/main/java/com/example/lab04_dante_gonzales/MainActivity.kt
+++ app/src/main/java/com/example/lab04_dante_gonzales/MainActivity.kt
@@ -6,22 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04_dante_gonzales.ui.theme.LAB04Dante_GonzalesTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             LAB04Dante_GonzalesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
                     Greeting(
-                        name = "Android",
+                        name = "Dante Gonzales",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +39,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Card(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Hola, $name",
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LAB04Dante_GonzalesTheme {
-        Greeting("Android")
+        Greeting(name = "Dante Gonzales")
     }
 }
